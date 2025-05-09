e0b9b1ba015777cb3bd5c9b3642fbfa52493e75d
James Scholes <james@jamesscholes.com>
2025-05-09 17:30:56 -0600

    Add Bandcamp userscript

---
+++ BandcampA11yFixes.user.js
@@ -0,0 +1,105 @@
+// ==UserScript==
+// @name Bandcamp Accessibility Fixes
+// @namespace      https://jscholes.github.io/
+// @description    Improves the accessibility of Bandcamp.
+// @author         James Scholes
+// @copyright 2025 James Scholes
+// @license Mozilla Public License version 2.0
+// @version        2025.1
+// @include https://*.bandcamp.com/*
+// ==/UserScript==
+
+/*** Code to apply the tweaks when appropriate. ***/
+
+function applyTweak(el, tweak) {
+	if (Array.isArray(tweak.tweak)) {
+		let [func, ...args] = tweak.tweak;
+		func(el, ...args);
+	} else {
+		tweak.tweak(el);
+	}
+}
+
+function applyTweaks(root, tweaks, checkRoot, forAttrChange = false) {
+	for (let tweak of tweaks) {
+		if (!forAttrChange || tweak.whenAttrChangedOnAncestor !== false) {
+			for (let el of root.querySelectorAll(tweak.selector)) {
+				try {
+					applyTweak(el, tweak);
+				} catch (e) {
+					console.log("Exception while applying tweak for '" + tweak.selector + "': " + e);
+				}
+			}
+		}
+		if (checkRoot && root.matches(tweak.selector)) {
+			try {
+				applyTweak(root, tweak);
+			} catch (e) {
+				console.log("Exception while applying tweak for '" + tweak.selector + "': " + e);
+			}
+		}
+	}
+}
+
+let observer = new MutationObserver(function (mutations) {
+	for (let mutation of mutations) {
+		try {
+			if (mutation.type === "childList") {
+				for (let node of mutation.addedNodes) {
+					if (node.nodeType != Node.ELEMENT_NODE) {
+						continue;
+					}
+					applyTweaks(node, DYNAMIC_TWEAKS, true);
+				}
+			} else if (mutation.type === "attributes") {
+				applyTweaks(mutation.target, DYNAMIC_TWEAKS, true, true);
+			}
+		} catch (e) {
+			// Catch exceptions for individual mutations so other mutations are still handled.
+			console.log("Exception while handling mutation: " + e);
+		}
+	}
+});
+
+function init() {
+	applyTweaks(document, LOAD_TWEAKS, false);
+	applyTweaks(document, DYNAMIC_TWEAKS, false);
+	options = { childList: true, subtree: true };
+	if (DYNAMIC_TWEAK_ATTRIBS.length > 0) {
+		options.attributes = true;
+		options.attributeFilter = DYNAMIC_TWEAK_ATTRIBS;
+	}
+	observer.observe(document, options);
+}
+
+/*** Define the actual tweaks. ***/
+
+// Tweaks that only need to be applied on load.
+const LOAD_TWEAKS = [
+];
+
+// Attributes that should be watched for changes and cause dynamic tweaks to be
+// applied.
+const DYNAMIC_TWEAK_ATTRIBS = [];
+
+// Tweaks that must be applied whenever an element is added/changed.
+const DYNAMIC_TWEAKS = [
+	{
+		selector: 'a[role="button"] > div:only-child',
+		tweak: el => {
+			const label = el.parentElement.ariaLabel;
+			el.parentElement.role = 'presentation';
+			el.parentElement.removeAttribute('aria-label');
+			el.ariaLabel = label;
+			el.tabIndex = '0';
+			el.role = 'button';
+		},
+	},
+];
+
+/** add your specific initialization here, so that if you ever update the framework from new skeleton your inits are not overridden. */
+function userInit(){}
+
+/*** Lights, camera, action! ***/
+init();
+userInit();
